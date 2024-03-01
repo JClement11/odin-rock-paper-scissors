522b545bf1c2778b08ca8c5b1aabaa517c38f378
JClement11 <jclement0829@gmail.com>
2024-03-01 17:00:33 -0600

    Change computer options to uppercase

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 function getComputerChoice() {
-    const options = ["Rock", "Paper", "Scissors"];
+    const options = ["ROCK", "PAPER", "SCISSORS"];
     let randomChoice = options[Math.floor(Math.random()*options.length)];
     return randomChoice;
 }
+
 function getPlayerChoice() {
     let playerChoice = prompt("Do you pick Rock, Paper, or Scissors?");
     playerChoice = playerChoice.toUpperCase();
     return playerChoice;
 }
+
+
+
 

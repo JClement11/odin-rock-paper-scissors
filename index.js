b3ca1e48d95f33a487d438deb2d2a6c4caa8e0c9
JClement11 <jclement0829@gmail.com>
2024-03-01 15:54:00 -0600

    Create function to randomize computer choice

---
+++ index.js
@@ -0,0 +1,5 @@
+function getComputerChoice() {
+    const options = ["Rock", "Paper", "Scissors"];
+    let randomChoice = options[Math.floor(Math.random()*options.length)];
+    return randomChoice;
+}
